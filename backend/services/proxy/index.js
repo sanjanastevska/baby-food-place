d2fb11f5933e78acdb521975e3f319e377a99e26
sanjanastevska <sanjanastevska@gmail.com>
2021-05-11 22:50:30 +0200

    add proxy server

---
+++ backend/services/proxy/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const proxy = require('express-http-proxy');
+const config = require('../../config/index');
+
+const app = express();
+
+app.use('/api/users', proxy(
+    `http://localhost:${config.get('ports').users}`,
+    { 
+        proxyReqPathResolver: (req) => {
+            return `http://localhost:${config.get('ports').users}/users${req.url}`
+        }
+    }
+));
+
+app.use('/api/recipes', proxy(
+    `http://localhost:${config.get('ports').recipes}`,
+    {
+        proxyReqPathResolver: (req) => {
+            return `http://localhost:${config.get('ports').recipes}/api/recipes${req.url}`
+        }
+    }
+));
+
+// app.use('/api/storage', proxy(
+//     `http://localhost:${config.get('ports').storage}`,
+//     {
+//         proxyReqPathResolver: (req) => {
+//             return `http://localhost:${config.get('ports').storage}/api/storage${req.url}`
+//         }
+//     }
+// ));
+
+// app.use('/', express.static(`${__dirname}/../../public/build`));
+
+const PORT = process.env.PORT || config.get('ports').proxy;
+
+app.listen(PORT, err => {
+    if(err) {
+        return console.log('Could not start proxy service', err);
+    }
+    console.log(`Proxy service successfully started on PORT: ${PORT}...`);
+});
